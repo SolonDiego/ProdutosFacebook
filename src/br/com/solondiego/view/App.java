80dd1b2597b9942462cda38553889204fdbdfbbe
Solon Diego Garcia Moreira <sdiegogm@gmail.com>
2021-04-06 18:30:54 -0300

    update teste

--- src/br/com/solondiego/view/App.java
+++ src/br/com/solondiego/view/App.java
@@ -10,7 +10,7 @@ import br.com.solondiego.model.Urls;
 public class App {
 
     public static void main(String[] args) {
-        int[] codigos = {1389,5409};             
+        int[] codigos = {1576,5465,5402};             
         GerarPlanilha gp = new GerarPlanilha(codigos);
     }
 }
